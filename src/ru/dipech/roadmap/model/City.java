b968ac4003dc4dc3d9504c28592b5f57fe449557
DiPech <dmitry.pechkovsky@gmail.com>
2019-09-04 10:49:40 +0300

    Initial commit. Создал каркасы основных моделей.

---
+++ src/ru/dipech/roadmap/model/City.java
@@ -0,0 +1,31 @@
+package ru.dipech.roadmap.model;
+
+import java.util.Objects;
+
+public class City {
+    private String name;
+    private float x;
+    private float y;
+
+    public City(String name, float x, float y) {
+        Objects.requireNonNull(name, "name must not be null");
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Float.compare(city.x, x) == 0 &&
+                Float.compare(city.y, y) == 0 &&
+                name.equals(city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, x, y);
+    }
+}
---
+++ src/ru/dipech/roadmap/model/Road.java
@@ -0,0 +1,33 @@
+package ru.dipech.roadmap.model;
+
+import java.util.Objects;
+
+public class Road {
+    private String name;
+    private City cityFrom;
+    private City cityTo;
+
+    public Road(String name, City cityFrom, City cityTo) {
+        Objects.requireNonNull(name, "name must not be null");
+        Objects.requireNonNull(cityFrom, "cityFrom must not be null");
+        Objects.requireNonNull(cityTo, "cityTo must not be null");
+        this.name = name;
+        this.cityFrom = cityFrom;
+        this.cityTo = cityTo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Road road = (Road) o;
+        return name.equals(road.name) &&
+                cityFrom.equals(road.cityFrom) &&
+                cityTo.equals(road.cityTo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, cityFrom, cityTo);
+    }
+}
